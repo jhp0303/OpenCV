a32811e1d4a06651a7fe3ec05368881420aac2ae
jhp0303 <yingouyingou@gmail.com>
2019-09-09 17:34:43 +0900

    Draw a white part square

--- pyTest.py
+++ pyTest.py
@@ -7,15 +7,71 @@ import pytesseract
 import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 
+img = cv.imread("C:/Users/Wolf/Desktop/ALPR/Image/bmw_Thresholding.jpg", cv.IMREAD_UNCHANGED)
+
+img2 = cv.imread("C:/Users/Wolf/Desktop/ALPR/Image/bmw_gray.jpg", cv.IMREAD_UNCHANGED)
+blur = cv.GaussianBlur(img2,(3,3),0)
+canny=cv.Canny(blur,100,200)
+
+cnts,contours,hierarchy  = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+box1 = []
+f_count = 0
+select = 0
+plate_width = 0
+
+for i in range(len(contours)):
+    cnt=contours[i]          
+    area = cv.contourArea(cnt)
+    x,y,w,h = cv.boundingRect(cnt)
+    rect_area=w*h  #area size
+    aspect_ratio = float(w)/h # ratio = width/height
+        
+    if  (aspect_ratio>=0.2)and(aspect_ratio<=1.0)and(rect_area>=100)and(rect_area<=700): 
+        cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
+        box1.append(cv.boundingRect(cnt))
+         
+for i in range(len(box1)): ##Buble Sort on python
+    for j in range(len(box1)-(i+1)):
+        if box1[j][0]>box1[j+1][0]:
+                temp=box1[j]
+                box1[j]=box1[j+1]
+                box1[j+1]=temp
+                
+#to find number plate measureing length between rectangles
+for m in range(len(box1)):
+    count=0
+    for n in range(m+1,(len(box1)-1)):
+        delta_x=abs(box1[n+1][0]-box1[m][0])
+        if delta_x > 150:
+                break
+        delta_y =abs(box1[n+1][1]-box1[m][1])
+        if delta_x ==0:
+                delta_x=1
+        if delta_y ==0:
+                delta_y=1           
+        gradient =float(delta_y) /float(delta_x)
+        if gradient<0.25:
+            count=count+1
+    #measure number plate size         
+    if count > f_count:
+        select = m
+        f_count = count
+        plate_width=delta_x
+cv.imwrite('Image/bmw_rectangle.jpg',img)
+cv.imshow('bmw_rectangle', img)
+cv.waitKey()
+
 #------------------------------------------------------------------------------------------------------#
 
 # Adaptive Thresholding Image
 
-img = cv.imread("C:/Users/Wolf/Desktop/ALPR/Image/bmw_maxContrast.jpg", cv.IMREAD_UNCHANGED)
-ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
-cv.imshow('BINARY_INV', thresh2)
-plt.show()
-cv.waitKey()
+# img = cv.imread("C:/Users/Wolf/Desktop/ALPR/Image/bmw_maxContrast.jpg", cv.IMREAD_UNCHANGED)
+# ret,thresh2 = cv.threshold(img,127,255,cv.THRESH_BINARY_INV)
+# cv.imshow('BINARY_INV', thresh2)
+# cv.imwrite('Image/bmw_Thresholding.jpg', thresh2) # Save the New Image
+# plt.show()
+# cv.waitKey()
 
 
 #------------------------------------------------------------------------------------------------------#
